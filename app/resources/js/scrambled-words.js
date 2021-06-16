4c114e1d561ce2b59f5933dab6707c8152e759cc
DimovDimo <d.dimov1996@gmail.com>
2021-06-16 20:16:43 +0300

    added button with id="scrambledWordsSaveFirstAndLastLetter"

---
+++ app/resources/js/scrambled-words.js
@@ -0,0 +1,6 @@
+document.getElementById("scrambledWordsSaveFirstAndLastLetter")
+    .addEventListener("click", scrambledWordsSaveFirstAndLastLetter);
+
+function scrambledWordsSaveFirstAndLastLetter(){
+    // [a-zA-Z]+|[^a-zA-Z]+
+}
