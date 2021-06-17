966fb33c49dd58818ad9b191173f0b3e7b974193
DimovDimo <d.dimov1996@gmail.com>
2021-06-17 14:42:29 +0300

    added regexInput

--- app/resources/js/scrambled-words.js
+++ app/resources/js/scrambled-words.js
@@ -1,6 +1,12 @@
+const regexInput = /[a-zA-Z]+|[^a-zA-Z]+/g;
+
 document.getElementById("scrambledWordsSaveFirstAndLastLetter")
     .addEventListener("click", scrambledWordsSaveFirstAndLastLetter);
 
-function scrambledWordsSaveFirstAndLastLetter(){
-    // [a-zA-Z]+|[^a-zA-Z]+
+function scrambledWordsSaveFirstAndLastLetter() {
+    let text = document.getElementById("input").value;
+    
+    let inputs = text.match(regexInput);
+
+    console.log(inputs);
 }
