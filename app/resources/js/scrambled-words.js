9781701690036de3bcf71daf9a3ca8511c1a1076
DimovDimo <d.dimov1996@gmail.com>
2021-06-20 12:01:47 +0300

    added function pseudoRandomizeArray

--- app/resources/js/scrambled-words.js
+++ app/resources/js/scrambled-words.js
@@ -5,16 +5,25 @@ document.getElementById("scrambledWordsSaveFirstAndLastLetter")
     .addEventListener("click", scrambledWordsSaveFirstAndLastLetter);
 
 function scrambledWordsSaveFirstAndLastLetter() {
-    let text = document.getElementById("input").value;    
+    let text = document.getElementById("input").value;
     let inputs = text.match(regexInput);
     let result = "";
     inputs.forEach(element => {
         if (regexIsWordWithMinLength.exec(element)) {
-            let middleItems = element.slice(1, element.length - 1);
+            let lastItem = element.length - 1;
+            let middleItems = element.slice(1, lastItem);
+            middleItems = pseudoRandomizeArray(middleItems);
+            result += (element[0] + middleItems + element[lastItem]);
         } else {
             result += element;
         }
     });
 
     console.log(result);
+}
+
+function pseudoRandomizeArray(array) {
+    return array.split('')
+        .sort(() => 0.5 - Math.random())
+        .join('');
 }
